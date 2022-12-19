d0fc589fc1b81b0978837bee85754bbd21db3b56
henrywu <henrw@umich.edu>
2022-12-19 15:41:53 -0500

    update

--- example/config.js
+++ example/config.js
@@ -1,28 +1,29 @@
 module.exports = {
   pathPrefix: '',
-  siteUrl: 'https://henrw.github.io/',
-  title: 'Muzhe Wu',
-  description: 'Personal Website of Muzhe Wu',
-  author: 'Muzhe Wu',
-  authorAlternative: '吴沐哲',
+  siteUrl: 'https://boyanzh.site',
+  title: 'BoYanZh',
+  description: 'Personal Website of BoYanZh',
+  author: 'Boming ZHANG',
+  authorAlternative: '张泊明',
   introduction: [
-    'I am currently a senior student studying for a bachelor\'s degree in Computer Science at [University of Michigan](https://umich.edu/) and a dual degree in Electrical and Computer Engineering at [Shanghai Jiao Tong University](https://en.sjtu.edu.cn/).',
-    'My research interest mainly lies in the interdisciplinary field of Human-Computer Interaction and Artificial Intelligence, in which I focus on creating intelligent interactive systems/agents that can learn from humans’ behaviors and respond in alignment with humans’ intents.',
-    'I’m fortunate to have worked with Professors [Anhong Guo](https://guoanhong.com/), [Xu Wang](https://web.eecs.umich.edu/~xwanghci/), [Veronica Perez Rosas](https://vrncapr.engin.umich.edu/) through my undergraduate research.'
+    'Hi there, I am BoYanZh (Boming Zhang). I am a software developer from China. 🇨🇳',
+    'I am keen on writing high-quality, readable, and maintainable codes with the help of check tools. Also, automating the whole process from development to deployment with CI/CD is one of my interests.',
+    'I like to build software with real-world impact and is accessible by everyone and everywhere. So I have taken part in plenty of Web projects, working mainly on the backend side with `Python3` and `Go`. I am trying to dive deeper and deeper into the backend side.',
+    'I have built lots of handy tools and platforms for my undergraduate institute. They mainly aim at making the life of students easier. Check this [Gist](https://gist.github.com/BoYanZh/fc4469c20fd6adf42c212114532aaac0) for a collection of useful tools built by other students and me!',
+    'Get my coding statistics here since entering college: [![wakatime](https://wakatime.com/badge/user/8dcab400-e2d4-43ad-8a8f-7a94a34ea7b6.svg)](https://wakatime.com/@8dcab400-e2d4-43ad-8a8f-7a94a34ea7b6).',
   ],
   avatar: 'avatar.png',
-  professions: [
-    'CSE Student',
-  ],
-  birthday: 'Some day',
-  location: 'Ann Arbor, MI',
-  email: 'henrw@umich.edu',
+  professions: ['Undergraduate Student', 'Full-Stack Developer'],
   tocMaxDepth: 2,
   excerptMaxLength: 500,
+  birthday: 'Mar. 16, 2000',
+  location: 'Shanghai, China',
+  email: 'bomingzh@sjtu.edu.cn',
+  language: '中文, English',
   postsForArchivePage: 3,
   defaultLanguage: 'en',
-  disqusScript: process.env.DISQUS_SCRIPT
-    || 'https://tc-imba.disqus.com/embed.js',
+  disqusScript:
+    process.env.DISQUS_SCRIPT || 'https://boyanzh.disqus.com/embed.js',
   pages: {
     home: '/',
     posts: 'posts',
@@ -36,180 +37,280 @@ module.exports = {
     {
       url: '/resume.pdf',
       icon: ['ai', 'cv'],
-    }, {
-      url: 'https://github.com/henrw',
+    },
+    {
+      url: 'https://github.com/BoYanZh',
       icon: ['fab', 'github'],
-    }, {
-      url: 'https://www.linkedin.com/in/muzhe-wu-573920223',
-      icon: ['fab', 'linkedin'],
-    }, {
-      url: 'https://twitter.com/MuzheW',
+    },
+    {
+      url: 'https://twitter.com/BoYanZh233',
       icon: ['fab', 'twitter'],
-    }
+    },
+    {
+      url: 'https://t.me/BoYanZh',
+      icon: ['fab', 'telegram'],
+    },
+    {
+      url: 'https://www.linkedin.com/in/boming-zhang-1aba46244/',
+      icon: ['fab', 'linkedin'],
+    },
+    {
+      url: 'https://steamcommunity.com/id/boyanzh',
+      icon: ['fab', 'steam'],
+    },
   ],
   // facebook: 'https://www.facebook.com/rolwin.monteiro',
   // instagram: 'https://www.instagram.com/reevan100/',
   // rss: '/rss.xml',
-  // wakatime: {
-  //   username: 'tcimba',
-  //   activity: '7add4047-08f9-4da8-b649-aa114503678f',
-  //   language: '460a84ab-722a-4b80-b896-cabaa13ad7eb',
-  //   editor: 'd851639a-28d8-4884-949f-d338a858f7e9',
-  //   os: 'caf7d0d1-8fd2-4595-a991-363c8583fea9',
-  // },
-  contactFormUrl: process.env.CONTACT_FORM_ENDPOINT
-    || 'https://getform.io/f/09a3066f-c638-40db-ad59-05e4ed71e451',
-  googleAnalyticTrackingId: process.env.GA_TRACKING_ID || '',
+  wakatime: {
+    username: 'boyanzh',
+    activity: '8ec8868a-3e3b-4d1d-845f-b1ba0a474e72',
+    language: '910c9181-965c-4d66-a371-c5d9ffeafbac',
+    editor: '489d2fc7-aa89-4984-af24-17115132d603',
+    os: '84d9c631-f6c1-4553-a0cc-cc3634936846',
+  },
+  contactFormUrl: process.env.CONTACT_FORM_ENDPOINT || '',
+  googleAnalyticTrackingId: process.env.GA_TRACKING_ID || 'UA-XXXXXXXXX-X',
   education: [
     {
-      date: 'Aug 2021 - Present',
+      date: 'Sept. 2018 - Aug. 2022',
       icon: 'university',
-      title: 'B.S. in Computer Science',
-      location: 'University of Michigan',
-    }, {
-      date: 'Sep 2019 - Aug 2021',
-      icon: 'university',
-      title: 'B.S. in Electrical and Computer Engineering',
-      location: 'Shanghai Jiao Tong University',
-    }],
+      title: 'B.S.E. in Electrical and Computer Engineering (minor in Computer Science)',
+      location: 'Shanghai Jiao Tong University (UM-SJTU Joint Institute)',
+    },
+    {
+      date: 'Sept. 2015 - June 2018',
+      icon: 'school',
+      title: 'High School',
+      location: 'High School Affiliated to Shanghai Jiao Tong University',
+    },
+  ],
   interests: [
     {
-      icon: 'universal-access',
-      title: 'Human-Computer Interaction',
-    },
-    {
-      icon: 'language',
-      title: 'Natural Language Processing',
-    },
-    {
       icon: 'layer-group',
-      title: 'Multimodal Learning',
-    }
+      title: 'Full Stack Development',
+    },
+    {
+      icon: 'network-wired',
+      title: 'Internet of Things',
+    },
+    {
+      icon: ['fab', 'docker'],
+      title: 'Container Technology',
+    },
+    {
+      icon: ['fab', 'linux'],
+      title: 'Open Source Community',
+    },
   ],
   experience: [
     {
-      title: 'Work',
+      title: 'Job',
       position: 'left',
       data: [
         {
-          date: 'May 2022 - Present',
-          title: 'Research Assistant',
-          location: 'University of Michigan',
-          description: 'Work in HAIL Lab and Lifelong Learning Lab with Prof. Anhong Guo and Xu Wang.',
+          date: 'Feb. 2022 - Jul. 2022',
+          title: 'Software Engineer Intern',
+          location: 'Naive Systems',
+          description: 'Build a code analyzer and relative DevSecOps products.',
         },
         {
-          date: 'Jul 2022 - Present',
-          title: 'Research Assistant',
-          location: 'University of Michigan',
-          description: 'Work in LIT Lab with Dr. Veronica Perez-Rosas.',
-        },
-        {
-          date: 'Jul 2022 - Sep 2022',
-          title: 'Research Assistant',
-          location: 'NVIDIA',
-          description: 'Work in Jim Team with Dr. Linxi "Jim" Fan',
-        },
+          date: 'June 2021 - Feb. 2022',
+          title: 'Research & Development Engineer Intern',
+          location: 'Shanghai Zhimian Weiye Technology Co., Ltd.',
+          description: 'Full-stack development engineer, technical leader.',
+        }
       ],
-    }, {
-      title: 'Competition',
+    },
+    {
+      title: 'Teaching Assistant',
       position: 'right',
       data: [
         {
-          date: 'Feb 2021',
-          title: 'Mathematical Contest in Modeling',
-          location: ' ',
-          description: 'Meritorious Winner Prize',
-        }, {
-          date: 'Nov 2020',
-          title: 'University Physics Competition',
-          location: ' ',
-          description: 'Silver Medal',
+          date: 'Sept. 2021 - Dec. 2021',
+          title: 'Teaching Assistant of Intro to OS',
+          location: 'Shanghai Jiao Tong University',
+          description: 'Advisor: Prof. Manuel Charlemagne'
+        },
+        {
+          date: 'Sept. 2020 - Dec. 2020',
+          title: 'Teaching Assistant of Intro to Comp.',
+          location: 'Shanghai Jiao Tong University',
+          description: 'Advisor: Prof. Manuel Charlemagne'
         },
       ],
-    }, {
-      title: 'Volunteer',
+    },
+    {
+      title: 'Laboratory',
       position: 'left',
       data: [
         {
-          date: 'Sept 2019 - Dec 2020',
-          title: 'JI Voluntary Association',
-          location: 'Jiangchuan Sunshine Nursing House, Shanghai',
-          description: 'Visit and help people with mental difficulties.'
-        }, {
-          date: 'May 2020',
-          title: 'High School Advisory',
-          location: 'No.2 Foreign Language School, Wenzhou',
-          description: 'Advise high school students on College Entrance Examination Prep',
-        }
+          date: 'Mar. 2021 - Feb. 2022',
+          title: 'Research Assistant',
+          location: 'Intelligent Design and Optimization Research Lab',
+          description: 'Advisor: Prof. Mian Li'
+        },
+        {
+          date: 'July 2021 – Sept. 2021',
+          title: 'Website Developer',
+          location: 'Advanced Network Laboratory',
+          description: 'Advisor: Prof. Xiaofeng Gao'
+        },
+        {
+          date: 'March 2019 – Dec. 2019',
+          title: 'Research Assistant',
+          location: 'Institute of Media, Information, and Network',
+          description: 'Advisor: Prof. Weiyao Lin'
+        },
+      ],
+    },
+    {
+      title: 'Student Organization',
+      position: 'right',
+      data: [
+        {
+          date: 'Sept. 2019 – Present',
+          title: 'Maintainer, Main Contributor',
+          location: 'Fans Of Computer Science',
+        },
+        {
+          date: 'Sept. 2018 – July 2019',
+          title: 'Deputy Director',
+          location:
+            'Technology Department UM-SJTU Joint Institute Student Union',
+        },
+        {
+          date: 'Sept. 2019 - July 2019',
+          title: 'Member',
+          location: 'Student Science, Technology and Innovation Association',
+        },
+      ],
+    },
+    {
+      title: 'Coursework',
+      position: 'left',
+      data: [
+        {
+          date: 'VE482',
+          location: 'Intro to Operating Systems (A+)',
+        },
+        {
+          date: 'VE472',
+          location: 'Methods and Tools for big data (A+)',
+        },
+        {
+          date: 'VE281',
+          location: 'Data Structures and Algorithms (A)',
+        },
+        {
+          date: 'VE280',
+          location: 'Programming and Elem. Data Structures (A+)',
+        },
+        {
+          date: 'VE450',
+          location: 'Major Design Experience (A)',
+        },
+        {
+          date: 'VE490',
+          location: 'Undergraduate Research (A)',
+        },
+        {
+          date: 'VG101',
+          location: 'Intro to Computers and Programming (A+)',
+        },
+        {
+          date: 'VG100',
+          location: 'Intro to Engineering (A)',
+        },
       ],
     },
   ],
   awards: [
     {
-      date: '2021 Fall, 2022 Winter',
-      title: 'University of Michigan Dean’s Honor List',
-    }, {
-      date: '2019-2020, 2020-2021',
-      title: 'SJTU Undergraduate Excellent Scholarship',
+      date: 'Nov. 2021',
+      title: '2020-2021 Undergraduate Excellent Scholarship',
+    },
+    {
+      date: 'June 2019',
+      title:
+        'Outstanding Member of Technology Department UM-SJTU Joint Institute Student Union',
     },
   ],
   tagColors: [
-    'magenta', 'red', 'volcano', 'orange', 'gold',
-    'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple',
+    'magenta',
+    'red',
+    'volcano',
+    'orange',
+    'gold',
+    'lime',
+    'green',
+    'cyan',
+    'blue',
+    'geekblue',
+    'purple',
   ],
   tags: [
     {
       id: 'javascript',
       name: 'javascript',
-      description: 'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.',
+      description:
+        'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.',
       color: '#f0da50',
     },
     {
       id: 'nodejs',
       name: 'Node.js',
-      description: 'Node.js is a tool for executing JavaScript in a variety of environments.',
+      description:
+        'Node.js is a tool for executing JavaScript in a variety of environments.',
       color: '#90c53f',
     },
     {
       id: 'rxjs',
       name: 'RxJS',
-      description: 'RxJS is a library for reactive programming using Observables, for asynchronous operations.',
+      description:
+        'RxJS is a library for reactive programming using Observables, for asynchronous operations.',
       color: '#eb428e',
     },
     {
       id: 'typescript',
       name: 'typescript',
-      description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
+      description:
+        'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
       color: '#257acc',
     },
     {
       id: 'reactjs',
       name: 'reactjs',
-      description: 'React is an open source JavaScript library used for designing user interfaces.',
+      description:
+        'React is an open source JavaScript library used for designing user interfaces.',
       color: '#61dbfa',
     },
     {
       id: 'gatsby',
       name: 'Gatsby.js',
-      description: 'A framework built over ReactJS to generate static page web application.  ',
+      description:
+        'A framework built over ReactJS to generate static page web application.  ',
       color: '#6f309f',
     },
     {
       id: 'html',
       name: 'HTML',
-      description: 'A markup language that powers the web. All websites use HTML for structuring the content.',
+      description:
+        'A markup language that powers the web. All websites use HTML for structuring the content.',
       color: '#dd3431',
     },
     {
       id: 'css',
       name: 'css',
-      description: 'CSS is used to style the HTML element and to give a very fancy look for the web application.',
+      description:
+        'CSS is used to style the HTML element and to give a very fancy look for the web application.',
       color: '#43ace0',
     },
     {
       id: 'python',
       name: 'python',
-      description: 'A general purpose programming language that is widely used for developing various applications.',
+      description:
+        'A general purpose programming language that is widely used for developing various applications.',
       color: '#f9c646',
     },
   ],
